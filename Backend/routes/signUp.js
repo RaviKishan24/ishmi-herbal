3f6f6f300245004b6753b6c5673085c78b742705
RaviKishan24 <Ravikishankumar71@gmail.com>
2026-02-18 12:47:22 +0530

    Remove all project files from repo and keep them local

--- Backend/routes/signUp.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signUp } from "../controller/signUp.js";
-const signUpRouter= express.Router();
-
-signUpRouter.post("/Register",signUp);
-
-export default signUpRouter;
-
-
